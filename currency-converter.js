65fa50fb576ece4e918a925ea63b9ec6cf4bc5ab
Mike Maat <mmaat@ualberta.ca>
2020-07-09 12:53:07 -0700

    Update currency-converter.js

--- currency-converter.js
+++ currency-converter.js
@@ -3,7 +3,8 @@ window.onload = function() {
 	$("span.wsite-com-product-price-amount").each(function() {
 		var foundPrice = $(this).html();
 		if (foundPrice.length > 0) {
-			console.log("Price Found!!!: " + foundPrice);
+			$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $0.00 USD)</span>" ).insertAfter($(this));
+			console.log("Price Found: " + foundPrice);
 		}
 	});
 }
