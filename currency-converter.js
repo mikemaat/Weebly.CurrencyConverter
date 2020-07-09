2332d54cb4c2ba866835dbc8da3738a7b8ffb993
Mike Maat <mmaat@ualberta.ca>
2020-07-09 14:08:09 -0700

    Update currency-converter.js

--- currency-converter.js
+++ currency-converter.js
@@ -3,6 +3,7 @@ var fixerAPIKey = "123";
 
 window.onload = function() {
 	$("span.wsite-com-product-price-amount").each(function() {
+		var priceSpan = $(this);
 		var foundPrice = $(this).html();
 		if (foundPrice.length > 0) {
 			console.log("Price Found: " + foundPrice);
@@ -13,12 +14,13 @@ window.onload = function() {
 				url: 'https://api.exchangeratesapi.io/latest',   
 				dataType: 'json',
 				success: function(json) {
-					console.log(json.rates.CAD);
-					console.log(json.rates.USD);
+					console.log("CAD: " + json.rates.CAD);
+					console.log("USD: " + json.rates.USD);
 					var conversionRate = json.rates.CAD / json.rates.USD;
-					console.log(conversionRate);
+					console.log("Conversion Rate: " + conversionRate);
 					var convertedPrice = parseFloat(rawPrice * conversionRate).toFixed(2);
-					$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $" + convertedPrice + " USD)</span>" ).insertAfter($(this));
+					console.log("Converted Price: " + convertedPrice);
+					$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $" + convertedPrice + " USD)</span>" ).insertAfter(priceSpan);
 				},
 				error: function(XHR, textStatus, errorThrown) {
 					console.log("error: " + textStatus);
