9f6412c70ddc8f426a3cb1e66ceae8385297703a
Mike Maat <mmaat@ualberta.ca>
2020-07-09 14:14:20 -0700

    Update currency-converter.js

--- currency-converter.js
+++ currency-converter.js
@@ -1,7 +1,8 @@
 // Currency Conversion Script
-var fixerAPIKey = "123";
-
 window.onload = function() {
+	var sourceCurrency = $("meta.priceCurrency").val;
+	console.log("Source Currency: " + sourceCurrency);
+	
 	$("span.wsite-com-product-price-amount").each(function() {
 		var priceSpan = $(this);
 		var foundPrice = $(this).html();
@@ -9,17 +10,12 @@ window.onload = function() {
 			console.log("Price Found: " + foundPrice);
 			var indexOfDollarSign = foundPrice.indexOf('$') + 1;
 			var rawPrice = foundPrice.substr(indexOfDollarSign, foundPrice.length - indexOfDollarSign);
-			
 			$.ajax({
 				url: 'https://api.exchangeratesapi.io/latest',   
 				dataType: 'json',
 				success: function(json) {
-					console.log("CAD: " + json.rates.CAD);
-					console.log("USD: " + json.rates.USD);
 					var conversionRate = json.rates.USD / json.rates.CAD;
-					console.log("Conversion Rate: " + conversionRate);
 					var convertedPrice = parseFloat(rawPrice * conversionRate).toFixed(2);
-					console.log("Converted Price: " + convertedPrice);
 					$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $" + convertedPrice + " USD)</span>" ).insertAfter(priceSpan);
 				},
 				error: function(XHR, textStatus, errorThrown) {
