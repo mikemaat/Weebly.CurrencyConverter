9d7bed2c01e813c2f20b3d58dbc432f628bcb3bc
Mike Maat <mmaat@ualberta.ca>
2020-07-09 13:25:57 -0700

    Update currency-converter.js

--- currency-converter.js
+++ currency-converter.js
@@ -1,13 +1,26 @@
 // Currency Conversion Script
+var fixerAPIKey = "123";
+
 window.onload = function() {
 	$("span.wsite-com-product-price-amount").each(function() {
 		var foundPrice = $(this).html();
 		if (foundPrice.length > 0) {
 			console.log("Price Found: " + foundPrice);
 			var indexOfDollarSign = foundPrice.indexOf('$') + 1;
-			var rawPrice = foundPrice.substr(indexOfDollarSign, foundPrice.length - indexOfDollarSign); 
-			var convertedPrice = parseFloat(rawPrice * 0.74).toFixed(2);
-			$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $" + convertedPrice + " USD)</span>" ).insertAfter($(this));
+			var rawPrice = foundPrice.substr(indexOfDollarSign, foundPrice.length - indexOfDollarSign);
+
+			$.ajax({
+			    url: 'https://data.fixer.io/api/latest?access_key=' + fixerAPIKey,   
+			    dataType: 'jsonp',
+			    success: function(json) {
+			    	alert(json.rates.CAD);
+			    	alert(json.rates.USD);
+			    	var conversionRate = json.rates.CAD / json.rates.USD;
+				alert(conversionRate);
+			    	var convertedPrice = parseFloat(rawPrice * conversionRate).toFixed(2);
+				$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $" + convertedPrice + " USD)</span>" ).insertAfter($(this));
+			    }
+			});
 		}
 	});
 }
