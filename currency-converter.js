37d3990c0e021c10dec37a4ea05bdd554debcff2
Mike Maat <mmaat@ualberta.ca>
2020-07-09 13:17:14 -0700

    Update currency-converter.js

--- currency-converter.js
+++ currency-converter.js
@@ -3,15 +3,11 @@ window.onload = function() {
 	$("span.wsite-com-product-price-amount").each(function() {
 		var foundPrice = $(this).html();
 		if (foundPrice.length > 0) {
-			
 			console.log("Price Found: " + foundPrice);
-			
 			var indexOfDollarSign = foundPrice.indexOf('$') + 1;
 			var rawPrice = foundPrice.substr(indexOfDollarSign, foundPrice.length - indexOfDollarSign); 
-			
-			console.log("Raw Price: " + rawPrice);
-			
-			$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $" + (rawPrice * 0.74) + " USD)</span>" ).insertAfter($(this));
+			var convertedPrice = parseFloat(rawPrice * 0.74).toFixed(2);
+			$("<span style=\"display: block; font-size: 0.5em; margin-top: 5px; color: #777777;\">(Approx $" + convertedPrice + " USD)</span>" ).insertAfter($(this));
 		}
 	});
 }
